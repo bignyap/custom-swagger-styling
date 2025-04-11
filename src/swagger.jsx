bd2074ad48390e9408d2d24644db67b60ad3b185
bignya <bignyaranjan@gmail.com>
2025-04-11 20:26:22 +0530

    Adding a custom style for the swagger ui docs

---
+++ src/swagger.jsx
@@ -0,0 +1,48 @@
+import React from "react"
+import SwaggerUI from 'swagger-ui-react'
+import 'swagger-ui-react/swagger-ui.css'
+import './custom-swagger-styles.css' // Import improved styles
+
+class AugmentingLayout extends React.Component {
+  render() {
+    const { getComponent } = this.props
+    const BaseLayout = getComponent("BaseLayout", true)
+
+    return (
+      <div>
+        <div className="myCustomHeader">
+          <h1>🚀 Welcome to My API Explorer</h1>
+        </div>
+        <BaseLayout />
+      </div>
+    )
+  }
+}
+
+const AugmentingLayoutPlugin = () => {
+  return {
+    components: {
+      AugmentingLayout: AugmentingLayout
+    },
+    wrapComponents: {
+      Topbar: () => () => null,
+      Models: () => () => null
+    }
+  }
+}
+
+const SwaggerUIComponent = ({ url }) => {
+    return (
+      <SwaggerUI
+        url={url}
+        plugins={[AugmentingLayoutPlugin]}
+        layout={"AugmentingLayout"}
+        docExpansion="none"
+        defaultModelsExpandDepth={-1}
+        defaultModelExpandDepth={0}
+      />
+    )
+  }
+  
+
+export default SwaggerUIComponent
